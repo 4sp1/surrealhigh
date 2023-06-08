efb8ca0d2aeae894266feeec33a9b6d1e48350f8
Malik Benkirane <freemalikbenkirane@gmail.com>
2023-06-08 09:25:45 +0100

    lib add NewIDFromThing (breaking change)
    Parse and Format surreal things with a new format not including
    unicode characters but the canonical surreal Record ID type.
    
    Now SelectDriver is moved to SurrealDriver and the subsequent driver
    is capable of Create operation.
    
    This commit drastically changes the current API proposed so far by
    surrealhigh so expect a lot of refactoring tasks.
    
    > Without annotation, text record IDs can contain letters,
    > numbers and _ characters.
    
    Read (surrealql/datamodel/ids)[
    https://surrealdb.com/docs/surrealql/datamodel/ids]
    
    Add NewIDFromThing which parse the Id from any Thing from any Table
    You can expect ErrNotInThisTable, ErrBadThing and uuid parse errors
    while you work with NewIDFromThing.
    
    Update Id stringer.
    
    Update tests.
    
    Remove method parse_id

--- lib.go
+++ lib.go
@@ -1,6 +1,13 @@
 package surrealhigh
 
-import "github.com/google/uuid"
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/rs/zerolog/log"
+)
 
 type Field string
 
@@ -17,8 +24,35 @@ func (t Thing) String() string {
 	return string(t)
 }
 
+var (
+	ErrNotInThisTable = errors.New("thing not in this table")
+	ErrBadThing       = errors.New("bad thing")
+)
+
+func NewIDFromThing(th Thing, tb Table) (_ Id, err error) {
+	rawId, found := strings.CutPrefix(string(th), tb.Prefix())
+	if !found {
+		fields := strings.FieldsFunc(th.String(), func(r rune) bool { return r == ':' })
+		if len(fields) != 2 {
+			err = fmt.Errorf("strings: len(colon fields)!=2: %w", ErrBadThing)
+			return
+		}
+		err = fmt.Errorf("strings: cut prefix: %w: %q not in %q; in %q", ErrNotInThisTable, string(th), string(tb), fields[0])
+		return
+	}
+	rawId = strings.ReplaceAll(rawId, "_", "-")
+	uid, err := uuid.Parse(rawId)
+	if err != nil {
+		log.Debug().Err(err).Msg("uuid: parse: " + rawId)
+		err = fmt.Errorf("uuid: parse: %w", err)
+		return
+	}
+	return Id(uid), nil
+}
+
 func (i Id) String() string {
-	return uuid.UUID(i).String()
+	canonic := strings.ReplaceAll(uuid.UUID(i).String(), "-", "_")
+	return canonic
 }
 
 type Table string
@@ -28,7 +62,7 @@ func (t Table) Prefix() string {
 }
 
 func (i Id) Thing(t Table) Thing {
-	return Thing(t.Prefix() + "`" + i.String() + "`")
+	return Thing(t.Prefix() + i.String())
 }
 
 func NewID() Id {
